cbd2c2cfd6adde69d5bde4a3cb80901a684835b3
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2016-08-02 14:57:30 +0300

    Add page class suffix

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
-<?php defined('_JEXEC') or die; ?>
+<?php
+
+defined('_JEXEC') or die;
+
+$app  = JFactory::getApplication();
+$menu = $app->getMenu()->getActive();
+
+$templateUrl = $this->baseurl . '/templates/' . $this->template;
+
+$pageClass = '';
+if (is_object($menu)) {
+    $pageClass = $menu->params->get('pageclass_sfx');
+}
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.css" type="text/css" />
 </head>
-<body>
+<body class="<?php echo $pageClass ? htmlspecialchars($pageClass) : ''; ?>">
     <?php if ($this->countModules('header')): ?>
         <header id="header" role="banner">
             <jdoc:include type="modules" name="header" />
